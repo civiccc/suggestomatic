361c2b3f7cf234eed790ca3b3b4c30903b2a4561
Adam Derewecki <adam@causes.com>
2011-09-01 15:20:03 -0700

    Refactored Suggestomatic class

--- suggestomatic.py
+++ suggestomatic.py
@@ -1,101 +1,41 @@
 import argparse
 import array
+import bisect
 import mmap
 import os
+import pickle
 import struct
 import sys
 import time
 
-class Suggestomatic:
-  def parseargs(self):
-    parser = argparse.ArgumentParser(
-      description="Suggestomatic calculation engine")
+def print_first_ten(filename, out_array):
+  print "First 10 elements in '%s' array:" % filename,
+  print ', '.join(str(element) for element in out_array[:10])
 
-    addarg = parser.add_argument
-    addarg('--set-ids-filename', type=str,
-      help='Binary file containing unsigned int_32s of set IDs')
-    addarg('--set-index-filename', type=str,
-      help='Binary array of unsigned int_32s that indexes set_membership_filename')
-    addarg('--set-members-filename', type=str,
-      help='Binary array or arrays of member_id unsigned int_32s')
-    addarg('--suggestions-filename', type=str,
-      help='Output filename prefix')
-    addarg('--good-threshold', type=float, default=.05)
-    addarg('--begin-at', type=int, default=0)
+def parseargs():
+  parser = argparse.ArgumentParser(
+    description="Suggestomatic calculation engine")
 
-    options = parser.parse_args()
-    #TODO check the options
-    return options
+  addarg = parser.add_argument
+  addarg('--set-index-filename', type=str,
+    help='Pickled dictionary of key => (start_offset, end_offset)')
+  addarg('--set-members-filename', type=str,
+    help='Binary array or arrays of member_id unsigned int_32s')
+  addarg('--suggestions-filename', type=str,
+    help='Output filename prefix')
+  addarg('--begin-at', type=int, default=0)
+  return parser.parse_args()
 
-  def run(self):
-    options = self.parseargs()
-    print options
-
-    def print_first_ten(filename, out_array):
-      print "First 10 elements in '%s' array:" % filename,
-      print ', '.join(str(element) for element in out_array[:10])
-
-    def load_binary_array(filename):
-      out_array = array.array('I')
-      with open(filename) as fh:
-        size = os.path.getsize(filename)
-        item_count = size / out_array.itemsize
-        out_array.fromfile(fh, item_count)
-        print_first_ten(filename, out_array)
-      return out_array
-
-    set_ids_array = load_binary_array(options.set_ids_filename)
-    set_index_array = load_binary_array(options.set_index_filename)
-
-
-    # load the large members array of arrays
-    with open(options.set_members_filename, 'rb') as fh:
-      set_members_mmap = mmap.mmap(fh.fileno(), 0, access=mmap.ACCESS_READ)
-
-    set_members_array = array.array('I')
-    first_ten = set_members_mmap.read(10 * set_members_array.itemsize)
-    set_members_array.fromstring(first_ten)
-    print_first_ten(options.set_members_filename, set_members_array)
-
-    def load_set_array(set_id, next_set_id):
-      start, end = [set_index_array[offset] for offset in (set_id, next_set_id)]
-      set_members_mmap.seek(start)
-      s = array.array('I')
-      s.fromstring(set_members_mmap.read(end - start))
-      return s
-
-    for j, set_a_id in enumerate(set_ids_array):
-      set_a_next_id = set_ids_array[j+1]
-      set_a = load_set_array(set_a_id, set_a_next_id)
-      set_a_length = float(len(set_a))
-      set_a_scores = []
-      start_time = time.time()
-      for i, set_b_id in enumerate(set_ids_array):
-        start = time.time()
-        timing = {}
-        set_b_next_id = set_ids_array[i+1]
-        set_b = load_set_array(set_b_id, set_b_next_id)
-        timing['loading'] = time.time() - start ; start = time.time()
-        intersections = bisect_intersection(set_a, set_b)
-        timing['intersections'] = time.time() - start ; start = time.time()
-        if set_a_length == 0: set_a_length = .0000001
-        score = intersections / set_a_length
-
-        if sum(timing.values()) > 1.0:
-          print 'Set b: %s, loading %.3f / calculating: %.3f' % (
-            len(set_b), timing['loading'], timing['intersections'])
-
-        if score > 1: import pdb; pdb.set_trace()
-        if score > 0:
-          set_a_scores.append((set_b_id, score))
-        if i % 1000 == 0: print i, time.time() - start_time
-
-      with open('suggestions.csv', 'w+') as result_fh:
-        for set_id, score in set_a_sorted(key=lambda x: x[1], reverse=True)[:25]:
-          result_fh.write('%s,%s\n' % (set_id, score))
+def load_binary_array(filename):
+  out_array = array.array('I')
+  with open(filename) as fh:
+    size = os.path.getsize(filename)
+    item_count = size / out_array.itemsize
+    out_array.fromfile(fh, item_count)
+    print_first_ten(filename, out_array)
+  return out_array
 
 # http://docs.python.org/library/bisect.html#searching-sorted-lists
-import bisect
 def bisect_index(haystack, needle):
     '''Locate the leftmost value exactly equal to needle'''
     i = bisect.bisect_left(haystack, needle)
@@ -105,6 +45,62 @@ def bisect_intersection(list_a, list_b):
     big_set = list_a if len(list_a) > len(list_b) else list_b
     small_set = list_a if len(list_a) <= len(list_b) else list_b
     return sum(1 for number in small_set if bisect_index(big_set, number))
+
+class Suggestomatic:
+  def load_set_array(self, start, end):
+    self.set_members_mmap.seek(start)
+    s = array.array('I')
+    s.fromstring(self.set_members_mmap.read(end - start))
+    return s
+
+  def score(self, set_a, set_b):
+    intersections = bisect_intersection(set_a, set_b)
+    return intersections / float(len(set_a))
+
+  def run(self):
+    options = parseargs()
+    print options
+
+    with open(options.set_index_filename, 'rb') as fh:
+      set_index = pickle.load(fh)
+      set_ids = sorted(set_index.keys())
+      print_first_ten("set ids", set_ids)
+
+    # load the large members array of arrays
+    with open(options.set_members_filename, 'rb') as fh:
+      self.set_members_mmap = mmap.mmap(fh.fileno(), 0, access=mmap.ACCESS_READ)
+
+    set_members_array = array.array('I')
+    first_ten = self.set_members_mmap.read(10 * set_members_array.itemsize)
+    set_members_array.fromstring(first_ten)
+    print_first_ten(options.set_members_filename, set_members_array)
+
+    for j, set_a_id in enumerate(set_ids[options.begin_at:]):
+      set_a = self.load_set_array(*set_index[set_a_id])
+      set_a_length = len(set_a)
+      set_a_scores = []
+      start_time = time.time()
+      for i, set_b_id in enumerate(set_ids):
+        start = time.time()
+        timing = {}
+        set_b = self.load_set_array(*set_index[set_b_id])
+        timing['loading'] = time.time() - start ; start = time.time()
+        score = self.score(set_a, set_b)
+        timing['scoring'] = time.time() - start ; start = time.time()
+
+        if sum(timing.values()) > 1.0:
+          print 'Set b: %s, loading %.3f / scoring: %.3f' % (
+            len(set_b), timing['loading'], timing['scoring'])
+
+        if score > 1: import pdb; pdb.set_trace()
+        if score > 0:
+          set_a_scores.append((set_b_id, score))
+        if i % 5000 == 0: print i, time.time() - start_time
+
+      print "Compared %d (size %d) to %d other sets" % (set_a_id, set_a_length, i)
+      with open('suggestions.csv', 'w+') as fh:
+        for set_id, score in sorted(set_a_scores, key=lambda x: x[1], reverse=True)[:25]:
+          fh.write('%s,%s,%s\n' % (set_a_id, set_id, score))
 
 if __name__ == '__main__':
   Suggestomatic().run()
